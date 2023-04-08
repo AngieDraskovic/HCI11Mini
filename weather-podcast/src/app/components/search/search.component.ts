aaac4efbe72aad5323633bf64cb2223e90ea91f1
serbedzijajovan <100165422+serbedzijajovan@users.noreply.github.com>
2023-04-08 21:30:28 +0200

    Autocomplete design finished

--- weather-podcast/src/app/components/search/search.component.ts
+++ weather-podcast/src/app/components/search/search.component.ts
@@ -1,41 +1,47 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { map, startWith } from 'rxjs/operators';
-import { WeatherService } from 'src/app/services/weather.service';
+import {HttpClient} from '@angular/common/http';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {map, startWith} from 'rxjs/operators';
+import {WeatherService} from 'src/app/services/weather.service';
+import {Observable} from "rxjs";
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
 
-  userInput:string = '';
-  cities: string[] = [];
-  filteredCities: string[] = [];
   cityControl = new FormControl();
-  weatherData:any;
-  astroData:any;
-  forecastData:any;
+  cities: string[] = ["Novi Sad", "Beograd", "Subotica"];
+  filteredCities: Observable<string[]> = new Observable<string[]>();
+  weatherData: any;
+  astroData: any;
+  forecastData: any;
   @Output() weatherDataEmitter = new EventEmitter<any>();
   @Output() astroDataEmitter = new EventEmitter<any>();
   @Output() forecastDataEmitter = new EventEmitter<any>();
   @Output() inputEmitter = new EventEmitter<any>();
 
 
-  constructor(private http: HttpClient, private weatherService: WeatherService){}
+  constructor(private http: HttpClient, private weatherService: WeatherService) {}
 
-  // ngOnInit(){
-  //   this.getCities();
-  //   this.cityControl.valueChanges
-  //     .pipe(
-  //       startWith(''),
-  //       map((value: string) => this.filterCities(value))
-  //     )
-  //     .subscribe(cities => {
-  //       this.filteredCities = cities;
-  //     });
-  // }
+  ngOnInit() {
+    this.filteredCities = this.cityControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+    // this.getCities();
+    // this.cityControl.valueChanges
+    //   .pipe(
+    //     startWith(''),
+    //     map((value: string) => this.filterCities(value))
+    //   )
+    //   .subscribe(cities => {
+    //     this.filteredCities = cities;
+    //   });
+  }
 
 
   filterCities(value: string): string[] {
@@ -45,7 +51,7 @@ export class SearchComponent {
 
 //   getCities() {
 //   const apiUrl = 'https://cors-anywhere.herokuapp.com/https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
- 
+
 // // Make API call to GeoDB Cities API through a proxy server
 //    this.http.get<any>(apiUrl, { headers: { 'X-Requested-With': 'XMLHttpRequest', 'X-RapidAPI-Key': 'your-rapidapi-key' } }).subscribe((data) => {
 //     this.cities = data.cities;
@@ -53,8 +59,20 @@ export class SearchComponent {
 
 // }
 
-  search(){
-    this.inputEmitter.emit(this.userInput);
-    
+  // search() {
+  //   this.inputEmitter.emit(this.userInput);
+  //
+  // }
+
+  private _filter(value: string): string[] {
+    if (value.length == 0) {
+      return [];
+    }
+
+    return this.cities.filter(cities => cities.toLowerCase().includes(value.toLowerCase()));
+  }
+
+  resetAutocomplete() {
+    this.cityControl.setValue('');
   }
 }
