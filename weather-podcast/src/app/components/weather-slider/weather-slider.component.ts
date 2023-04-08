09631d91fa35768a439adbf34fb9cdded1aaedd1
DusanPejanovic <pejanovic.dusan2001@gmail.com>
2023-04-08 23:43:06 +0200

    finished basic window slider

--- weather-podcast/src/app/components/weather-slider/weather-slider.component.ts
+++ weather-podcast/src/app/components/weather-slider/weather-slider.component.ts
@@ -1,4 +1,14 @@
-import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+
+
+
+interface WeatherDay {
+  date: string;
+  maxTemp: number;
+  minTemp: number;
+  conditionIcon: string;
+  conditionText: string;
+}
 
 @Component({
   selector: 'app-weather-slider',
@@ -6,17 +16,20 @@ import { Component, Input, SimpleChanges, ViewChild } from '@angular/core';
   styleUrls: ['./weather-slider.component.css']
 })
 export class WeatherSliderComponent {
-  @Input() tenDaysData:any;
+  @Input() tenDaysData: any;
+
+  weatherData: WeatherDay[] = [];
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['tenDaysData']) {
-      console.log("ng");
-      console.log(this.tenDaysData);
-      console.log("ng");
-
+      const tenDays = this.tenDaysData.forecast.forecastday.slice(0, 10);
+      this.weatherData = tenDays.map((day: any) => ({
+        date: day.date,
+        maxTemp: day.day.maxtemp_c,
+        minTemp: day.day.mintemp_c,
+        conditionIcon: day.day.condition.icon,
+        conditionText: day.day.condition.text
+      }));
     }
   }
-
-
-
 }
