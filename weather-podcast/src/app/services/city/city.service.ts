a52070fad9a9c76ac0a499f4e226612bc65dda17
serbedzijajovan <100165422+serbedzijajovan@users.noreply.github.com>
2023-04-10 17:48:25 +0200

    Autocomplete reworked

--- weather-podcast/src/app/services/city/city.service.ts
+++ weather-podcast/src/app/services/city/city.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {catchError, map, Observable, tap, throwError} from "rxjs";
+import {City} from "../../model/City";
+import {IpAddress} from "../../model/IpAddress";
+import {CurrentLocation} from "../../model/CurrentLocation";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
+  private apiUrl = 'https://api.weatherapi.com/v1';
+  private API_KEY = '7fbd719d147b4d468de150631230904'
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getCurrentLocation(): Observable<CurrentLocation> {
+    return this.http.get<CurrentLocation>(`${this.apiUrl}/ip.json?key=${this.API_KEY}&q=auto:ip`);
+  }
+
+  getCitiesForSearchTerm(searchTerm: string): Observable<City[]> {
+    return this.http.get<City[]>(`${this.apiUrl}/search.json?key=${this.API_KEY}&q=${searchTerm}`);
+  }
 }
